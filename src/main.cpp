6b5363cba3da294c035668f7ead8911d1acbad6c
Paul Schaber <paul.schaber@student.tabyenskilda.se>
2024-03-01 22:39:49 +0100

    After trying heaps of things to get it to work, it now works.
    I shall not question it further.

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,30 @@
 #include <Arduino.h>
 #include <sbus.h>
 
-#define RX_pin = 20
-#define TX_pin = 21
+#define RX_pin 20
+#define TX_pin 21
+
+#include <HardwareSerial.h>
+
+HardwareSerial SBUSS(0);
 
 /* SBUS object, reading SBUS */
-bfs::SbusRx sbus_rx(&Serial2);
+bfs::SbusRx sbus_rx(&SBUSS, 20, 21, true);
 /* SBUS object, writing SBUS */
-bfs::SbusTx sbus_tx(&Serial2);
+//bfs::SbusTx sbus_tx(&SBUSS, 22, 23, true);
 /* SBUS data */
 bfs::SbusData data;
 
 void setup() {
     /* Serial to display data */
     Serial.begin(115200);
-    while (!Serial) {}
+    //while (!Serial) {}
+    delay(1000);
+    Serial.println("-------------------------");
+    Serial.println("Setup");
+    Serial.println("-------------------------");
     /* Begin the SBUS communication */
     sbus_rx.Begin();
-    sbus_tx.Begin();
 }
 
 void loop(){
@@ -33,9 +40,5 @@ void loop(){
         Serial.print(data.lost_frame);
         Serial.print("\t");
         Serial.println(data.failsafe);
-        /* Set the SBUS TX data to the received data */
-        sbus_tx.data(data);
-        /* Write the data to the servos */
-        sbus_tx.Write();
     }
 }
